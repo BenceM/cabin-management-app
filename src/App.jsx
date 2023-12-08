f1e3fd4485842a1b7495e284f24a26bd6793d4b8
Bence <m.bence211@gmail.com>
2023-12-08 18:43:15 +0100

    basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import { useState } from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+	font-size: 30px;
+	font-weight: 600;
+	background-color: yellow;
+`;
+
+// to style app, style the div
+
+const StyledApp = styled.div`
+	background-color: orangered;
+	padding: 20px;
+`;
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<GlobalStyles />
+			<StyledApp>
+				<H1>The Wild Oasis</H1>
+				<Button>Check in</Button>
+				<Input type="number" placeholder="number of guests" />
+			</StyledApp>
+		</>
+	);
 }
 
 export default App;
