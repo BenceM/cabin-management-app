cf2807ebf85aff40a71d8a5e057617a42da5618d
Bence <m.bence211@gmail.com>
2023-12-09 22:26:23 +0100

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,44 @@
-import { useState } from "react";
-import styled from "styled-components";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+	Navigate,
+} from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-// to style app, style the div
-
-const StyledApp = styled.div`
-	padding: 20px;
-`;
-
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 function App() {
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<>
+				<Route element={<AppLayout />}>
+					<Route index element={<Navigate replace to="dashboard" />} />
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="bookings" element={<Bookings />} />
+					<Route path="cabins" element={<Cabins />} />
+					<Route path="users" element={<Users />} />
+					<Route path="settings" element={<Settings />} />
+					<Route path="account" element={<Account />} />
+				</Route>
+
+				<Route path="login" element={<Login />} />
+				<Route path="*" element={<PageNotFound />} />
+			</>,
+		),
+	);
 	return (
 		<>
 			<GlobalStyles />
-			<StyledApp>
-				<Row>
-					<Row type="horizontal">
-						<Heading as="h1">The Wild Oasis</Heading>
-						<div>
-							<Heading as="h2">Th</Heading>
-							<Button variation="primary" size="medium">
-								Check in
-							</Button>
-							<Button variation="secondary" size="small">
-								Check out
-							</Button>
-						</div>
-					</Row>
-					<Row>
-						<Heading as="h3">Form</Heading>
-						<form action="">
-							<Input type="number" placeholder="number of guests" />
-							<Input type="number" placeholder="number of guests" />
-						</form>
-					</Row>
-				</Row>
-			</StyledApp>
+			<RouterProvider router={router}></RouterProvider>;
 		</>
 	);
 }
