11490dfc638e6918229611d1c3d450aa59e50762
Bence <m.bence211@gmail.com>
2024-04-06 21:45:26 +0200

    add cabin functionality

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -6,7 +6,9 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { useForm } from "react-hook-form";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import { toast } from "react-hot-toast";
 const FormRow = styled.div`
 	display: grid;
 	align-items: center;
@@ -44,8 +46,20 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-	const { register, handleSubmit } = useForm();
-	function onSubmit(data) {}
+	const { register, handleSubmit, reset } = useForm();
+	const queryClient = useQueryClient();
+	const { mutate, isLoading } = useMutation({
+		mutationFn: createCabin,
+		onSuccess: () => {
+			toast.success("New cabin created");
+			queryClient.invalidateQueries({ queryKey: ["cabins"] });
+			reset();
+		},
+		onError: (err) => toast.error(err.message),
+	});
+	function onSubmit(data) {
+		mutate(data);
+	}
 	return (
 		<Form onSubmit={handleSubmit(onSubmit)}>
 			<FormRow>
@@ -93,7 +107,7 @@ function CreateCabinForm() {
 				<Button variation="secondary" type="reset">
 					Cancel
 				</Button>
-				<Button>Edit cabin</Button>
+				<Button disabled={isLoading}>Add cabin</Button>
 			</FormRow>
 		</Form>
 	);
