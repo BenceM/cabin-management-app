9901cb76f51a3bda2f887200ceec2d4499fbf1e0
Bence <m.bence211@gmail.com>
2024-06-29 20:30:49 +0200

    sorting functionality bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,17 +5,24 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
 	const [searchParams] = useSearchParams();
 	const filterValue = searchParams.get("status");
+	//filter
 	const filter =
 		!filterValue || filterValue === "all"
 			? null
 			: { field: "status", value: filterValue, method: "eq" };
+
+	//sort
+	const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+
+	const [field, direction] = sortByRaw.split("-");
+	const sortBy = { field, direction };
 	const {
 		isLoading,
 		data: bookings,
 		error,
 	} = useQuery({
-		queryKey: ["bookings", filter],
-		queryFn: () => getBookings({ filter }),
+		queryKey: ["bookings", filter, sortBy],
+		queryFn: () => getBookings({ filter, sortBy }),
 	});
 	return { isLoading, error, bookings };
 }
