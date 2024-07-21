6f53ba1ee4820ea2a3ddae10a3616f8a55bdbec9
Bence <m.bence211@gmail.com>
2024-07-21 20:27:11 +0200

    route protection, authorization

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+	height: 100vh;
+	background-color: var(--color-grey-50);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+	const navigate = useNavigate();
+	//load the auth user
+	const { user, isLoading, isAuthenticated } = useUser();
+
+	// if there is no auth user redirect to login
+
+	useEffect(() => {
+		if (!isAuthenticated && !isLoading) navigate("/login");
+	}, [isAuthenticated, isLoading, navigate]);
+	//while loading spinner
+	if (isLoading)
+		return (
+			<FullPage>
+				<Spinner />;
+			</FullPage>
+		);
+	// if user OK render app
+
+	if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
