02b080f003df3e0dd67f5f8bb0597e6fc00cfe83
Bence <m.bence211@gmail.com>
2024-07-26 16:04:42 +0200

    dashboard filter and layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: auto 34rem auto;
+	gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+	return (
+		<StyledDashboardLayout>
+			<div className="">Statistics</div>
+			<div className="">Activity of today</div>
+			<div className="">Stay durations</div>
+			<div className="">chart of sales</div>
+		</StyledDashboardLayout>
+	);
+}
+
+export default DashboardLayout;
