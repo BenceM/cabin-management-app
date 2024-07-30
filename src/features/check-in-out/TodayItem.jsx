e45b67ecfa679d379fe50a0c2dfff70483df2e36
Bence <m.bence211@gmail.com>
2024-07-30 15:51:06 +0200

    added checkin and out buttons

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -3,6 +3,7 @@ import Tag from "../../ui/Tag";
 import { Flag } from "../../ui/Flag";
 import Button from "../../ui/Button";
 import { Link } from "react-router-dom";
+import CheckoutButton from "./CheckoutButton";
 
 const StyledTodayItem = styled.li`
 	display: grid;
@@ -25,6 +26,7 @@ const Guest = styled.div`
 
 function TodayItem({ activity }) {
 	const { id, status, guests, numNights } = activity;
+	console.log(status);
 	return (
 		<StyledTodayItem>
 			{status === "unconfirmed" && <Tag type="green">Arriving</Tag>}
@@ -32,6 +34,17 @@ function TodayItem({ activity }) {
 			<Flag src={guests.countryFlag} alt={`Flag of ${guests.country}`} />
 			<Guest>{guests.fullName}</Guest>
 			<div className="">{numNights} nights</div>
+			{status === "unconfirmed" && (
+				<Button
+					size="small"
+					variation="primary"
+					as={Link}
+					to={`/checkin/${id}`}
+				>
+					Check in
+				</Button>
+			)}
+			{status === "checked-in" && <CheckoutButton bookingId={id} />}
 		</StyledTodayItem>
 	);
 }
